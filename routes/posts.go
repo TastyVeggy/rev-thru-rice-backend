f2983953ffa3a7558fc844906fd43e9b1a600283
Tasty <alexius2111@gmail.com>
2024-12-29 15:44:43 +0800

    stop exposing standalone shop creation api

--- routes/posts.go
+++ routes/posts.go
@@ -3,7 +3,6 @@ package routes
 import (
 	"github.com/TastyVeggy/rev-thru-rice-backend/controllers/comment"
 	"github.com/TastyVeggy/rev-thru-rice-backend/controllers/post"
-	"github.com/TastyVeggy/rev-thru-rice-backend/controllers/shop"
 	"github.com/labstack/echo/v4"
 )
 
@@ -22,6 +21,7 @@ func ProtectedPostRoutes(g *echo.Group) {
 	g.DELETE("/:id", post.DeletePost)
 	g.POST("/:id/comments", comment.CreateComment)
 
-	g.POST("/:id/shops", shop.CreateShop)
+	// user cannot create shop individually
+	// g.POST("/:id/shops", shop.CreateShop)
 	g.POST("/shop_posts", post.CreateShopPost) // Creating a shop comes bundled with a post and a rating
 }
